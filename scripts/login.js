c0e861adc73cf87de4c2dbe991e44cc0f04fe6b3
Moscolape <chunkums088@gmail.com>
2023-08-24 21:39:18 +0100

    add authentication feature and change project folder structure

--- scripts/login.js
+++ scripts/login.js
@@ -18,3 +18,63 @@ handleInputFocusBlur(adInput, adUserIcon);
 const adLock = document.getElementById('ad-lock');
 const lockIcon = document.getElementById('lock');
 handleInputFocusBlur(adLock, lockIcon);
+
+
+const loginForm = document.getElementById('login-form');
+
+
+loginForm.addEventListener('submit', (event) => {
+    // Prevent form submission
+    event.preventDefault();
+
+    // Get the values of the input fields
+    let email = document.getElementById('ad-input').value;
+    let password = document.getElementById('ad-lock').value;
+
+    const apiUrl = "https://embraceher.onrender.com";
+    const endpoint = "/api/v1/admin/login";
+    const url = apiUrl + endpoint;
+
+    console.log(url);
+
+    // Sample login data
+    const loginData = {
+        email: email,
+        password: password
+    };
+
+    console.log(loginData);
+
+    // Make the POST request
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(user => {
+        console.log("Login Response:", user);
+        console.log("User Token:", user.token);
+        
+        // Clear email and password fields
+        document.getElementById('ad-input').value = '';
+        document.getElementById('ad-lock').value = '';
+        
+        // Get the user's token
+        const token = user.token;
+
+        // Redirect to another page and pass the token as a query parameter
+        window.location.href = `dashboard.html?token=${token}`;
+
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+})
---
+++ scripts/user.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the token from the URL query parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+    console.log("Token from URL:", token);
+
+    // Get the token from localStorage
+    const getToken = localStorage.getItem('token');
+
+    if (token || getToken) {
+        const apiUrl = "https://embraceher.onrender.com";
+        const endpoint = "/api/v1/admin/me";
+        const url = apiUrl + endpoint;
+
+        fetch(url, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${getToken || token}`
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Request failed with status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(user => {
+            console.log("User Data:", user);
+            const adName = document.getElementById('ad-name');
+            const adEmail = document.getElementById('ad-email');
+
+            // Store the token in localStorage
+            localStorage.setItem('token', user.token);
+
+            adName.innerHTML = 'Admin Level' + ' ' + user.data.level;
+            adEmail.innerHTML = user.data.email;
+            adEmail.style.fontSize = '12px';
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+    }
+});
