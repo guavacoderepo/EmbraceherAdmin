510871c99ee3067233698ae5c82c35ebbb431c3d
Moscolape <chunkums088@gmail.com>
2023-09-17 20:06:02 +0100

    adopt the use of cloudinary url for the images

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,18 +1,19 @@
-// const myWidget = cloudinary.createUploadWidget({
-//     cloudName: 'mosco-cloud', 
-//     uploadPreset: 'uw-test'}, (error, result) => { 
-//         if (!error && result && result.event === "success") { 
-//             console.log('Done! Here is the image info: ', result.info); 
-//             console.log('Done! Here is the image url: ', result.info.secure_url); 
-//         } else {
-//             console.log('error:', error)
-//         }
-//     }
-// );
+const myWidget = cloudinary.createUploadWidget({
+    cloudName: 'mosco-cloud', 
+    uploadPreset: 'uw-test'}, (error, result) => { 
+        if (!error && result && result.event === "success") { 
+            console.log('Done! Here is the image info: ', result.info); 
+            console.log('Done! Here is the image url: ', result.info.secure_url);
+            document.getElementById('table-image-src').value = result.info.secure_url; 
+        } else {
+            console.log('error:', error)
+        }
+    }
+);
 
-// document.getElementById("add").addEventListener("click", function(){
-//     myWidget.open();
-// }, false);
+document.querySelector(".cloud").addEventListener("click", function(){
+    myWidget.open();
+}, false);
 
 
 const modal = document.getElementById('modal');
